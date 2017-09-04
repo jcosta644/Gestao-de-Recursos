560da712c0f683bfbaa228d5f72f5c6867b48784
Jardel Costa <jfcosta@ic.ufal.br>
2017-09-04 10:41:15 -0300

    adicionar primeira opcao

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import utility.*;
+import utility.GeneralPrints;
 
 /**
  * Created by HP on 03/09/2017.
@@ -7,11 +7,11 @@ public class App {
 
     public static void main(String[] args) {
         GeneralPrints gp = new GeneralPrints();
-        GeneralScanners sc = new GeneralScanners();
         int opc;
+
+
         do {
-            gp.printMenu();
-            opc = sc.scannerInt.nextInt();
+            opc = gp.printMenu();
             switch(opc) {
                 case 1:
                     break;
--- src/utility/GeneralPrints.java
+++ src/utility/GeneralPrints.java
@@ -1,10 +1,14 @@
 package utility;
 
+import utility.GeneralScanners;
+
 /**
  * Created by HP on 03/09/2017.
  */
 public class GeneralPrints {
-    public void printMenu() {
+    public int printMenu() {
+        GeneralScanners sc = new GeneralScanners();
+
         System.out.println(
                 "Menu:\n" +
                         "1 - Cadastrar Usuario\n" +
@@ -17,5 +21,17 @@ public class GeneralPrints {
                         "8 - Relatorio Completo\n" +
                         "0 - Sair\n" +
                         "Escolha uma opcao: ");
+        return sc.scannerInt.nextInt();
+    }
+
+    public void printRegisterUser() {
+        GeneralScanners sc = new GeneralScanners();
+
+        System.out.println("Digite o nome do usuario:\n");
+        sc.scannerString.nextLine();
+        System.out.println("Digite o e-mail do usuario:\n");
+        sc.scannerString.nextLine();
+        System.out.println("Digite a funcao(0 = Aluno, 1 = Professor, 2 = Pesquisador) do usuario:\n");
+        sc.scannerInt.nextInt();
     }
 }
